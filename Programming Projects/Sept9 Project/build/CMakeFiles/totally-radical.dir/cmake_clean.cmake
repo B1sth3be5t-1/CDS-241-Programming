c8e52cb98e87991946e06e4fbc0c243e33afacbe
Brian Myers <brianmyers691@gmail.com>
2022-09-08 10:37:50 -0400

    first commit

---
+++ Programming Projects/Sept9 Project/build/CMakeFiles/totally-radical.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/totally-radical.dir/main.cpp.o"
+  "CMakeFiles/totally-radical.dir/main.cpp.o.d"
+  "totally-radical"
+  "totally-radical.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/totally-radical.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Programming Projects/Sept9 Project/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/LVC/bjm008/Documents/CDS 241 Programming/Programming Projects/Sept9 Project
+# Build directory: /home/LVC/bjm008/Documents/CDS 241 Programming/Programming Projects/Sept9 Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
