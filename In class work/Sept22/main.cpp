141d21453707e796befa3af456240634ae97fadd
Brian Myers <brianmyers691@gmail.com>
2022-09-22 10:48:46 -0400

    Classwork 9/22

--- In class work/Sept22/main.cpp
+++ In class work/Sept22/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+using namespace std;
+
+int main() {
+
+    vector<int> nums {1,4,6,8,10,11,12};
+    auto it = nums.begin();
+    while (*it++ % 2 == 0);
+
+    --it;
+    cout << *it << endl;
+
 }
